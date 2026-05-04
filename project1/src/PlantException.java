6332d0feb6134c51a9641aed0ae3d39ffb914fd4
František Maxián <maxiafra@fit.cvut.cz>
2026-05-04 13:45:04 +0200

    Add domain exception type

---
+++ project1/src/PlantException.java
@@ -0,0 +1,10 @@
+public class PlantException extends Exception {
+    public PlantException(String message) {
+        super(message);
+    }
+
+    public PlantException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
